004267faedf18d268953969de840439615ef89a0
Matejj2002 <matej.gornals@gmail.com>
2025-04-16 16:19:53 +0200

    vymazanie nepotrebnych suborov

--- config.py
+++
@@ -1,5 +0,0 @@
-filter_type_question = {
-    "Matching Question" : "matching_answer_question",
-    "Short Question" : "short_answer_question",
-    "Multiple Choice Question": "multiple_answer_question"
-}
